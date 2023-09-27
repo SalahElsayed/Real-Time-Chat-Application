0527c9d465f7bc884972cb4bff71351dc53b9ada
Salah Abuhashem <salahelsayed995@gmail.com>
2023-09-27 06:50:44 +0300

    Adding AuthMiddleWareStack

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,49 +1,58 @@
-import json 
-from channels.generic.websocket import WebsocketConsumer 
-from asgiref.sync import async_to_sync 
-from .models import Room 
+import json
 
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+
+from .models import Room, Message  
 
 
 class ChatConsumer(WebsocketConsumer):
 
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
-        self.room_name = None 
-        self.room_group_name = None 
-        self.room = None 
-
+        self.room_name = None
+        self.room_group_name = None
+        self.room = None
+        self.user = None  # new
 
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
         self.room = Room.objects.get(name=self.room_name)
+        self.user = self.scope['user']  
 
-        # Connection Accepted 
+        # connection has to be accepted
         self.accept()
 
-        # Join the Group 
-        async_to_sync(self.channel_layer.group_add) (self.room_group_name, self.channel_name)
+        # join the room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name,
+        )
 
-
-    def disconnect(self, code):
-        async_to_sync(self.channel_layer.group_discard) (self.room_group_name, self.channel_name)
-
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name,
+        )
 
     def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
+        if not self.user.is_authenticated:  
+            return                          
+
         # send chat message event to the room
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
-                'type' : 'chat_message', 
-                'message' : message
+                'type': 'chat_message',
+                'user': self.user.username,  
+                'message': message,
             }
-
         )
+        Message.objects.create(user=self.user, room=self.room, content=message)  # new
 
-    
     def chat_message(self, event):
         self.send(text_data=json.dumps(event))
--- project/asgi.py
+++ project/asgi.py
@@ -1,17 +1,18 @@
-
 import os
 
-from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack  # new import
 from channels.routing import ProtocolTypeRouter, URLRouter
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+from django.core.asgi import get_asgi_application
+
 import chat.routing
 
-application = ProtocolTypeRouter(
-    {
-        'http' : get_asgi_application(),
-        'websocket': URLRouter(
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+application = ProtocolTypeRouter({
+  'http': get_asgi_application(),
+  'websocket': AuthMiddlewareStack(  
+        URLRouter(
             chat.routing.websocket_urlpatterns
         )
-    }
-)
-
+    ),  
+})
